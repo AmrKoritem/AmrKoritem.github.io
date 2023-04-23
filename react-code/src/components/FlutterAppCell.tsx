eac99374bec3beea7af90cd8857febb7e5c5b001
Amr Koritem <amr.koritem92@gmail.com>
2023-04-24 00:00:20 +0200

    adding the download app buttons for flutter apps

--- react-code/src/components/FlutterAppCell.tsx
+++ react-code/src/components/FlutterAppCell.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import playStoreBadge from "../assets/image/google-play-badge.png"
 import { FlutterApp } from "../models/FlutterApp"
 import AppImage from "./AppImage"
 
@@ -11,9 +12,25 @@ const FlutterAppCell: React.FC<{app: FlutterApp}> = (props) => {
         marginLeft: "20px"
     }
     return (
-        <div key={props.app.name} style={cellStyle}>
+        <div style={cellStyle}>
             <AppImage link={props.app.image.link} alt={props.app.image.alt} />
             <p>{props.app.name}</p>
+            <a
+            href={props.app.links.app_store}
+            style={{display: "inlineBlock", overflow: "hidden"}}>
+                <img
+                src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1625097600"
+                alt="Download on the App Store"
+                style={{borderRadius: "6px", width: "125px", height: "41px"}} />
+            </a>
+            <a
+            href={props.app.links.play_store}
+            style={{display: "inlineBlock", overflow: "hidden"}}>
+                <img
+                src={playStoreBadge}
+                alt="Download on the Play Store"
+                style={{borderRadius: "6px", width: "125px", height: "41px"}} />
+            </a>
         </div>
     )
 }
