902e5aac59246966799413bfcd8510b8feba84a4
Amr Koritem <amr.koritem92@gmail.com>
2023-04-23 23:58:49 +0200

    adding border to unavailable app images

--- react-code/src/components/AppImage.tsx
+++ react-code/src/components/AppImage.tsx
@@ -3,7 +3,15 @@ import underDevelopment from "../assets/image/under-development.png"
 
 const AppImage: React.FC<{link: string, alt: string}> = (props) => {
     let link = props.link === "" ? underDevelopment : props.link
-    let imageStyle: React.CSSProperties = { width: "100px", height: "100px", borderRadius: "22%" }
+    let imageStyle: React.CSSProperties = {
+        width: "100px",
+        height: "100px",
+        borderRadius: "22%",
+        borderStyle: "solid",
+        borderWidth: "thin",
+        padding: "10px",
+    }
+    imageStyle.borderColor = props.link === "" ? "gray" : "transparent"
     return (
         <img  src={link} alt={props.alt} style={imageStyle} />
     )
