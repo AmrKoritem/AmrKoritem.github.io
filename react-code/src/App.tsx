f16ddd1e21508c80185471edbaa6937438f06e9b
Amr Koritem <amr.koritem92@gmail.com>
2023-04-27 11:49:43 +0200

    info header done

--- react-code/src/App.tsx
+++ react-code/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import amrKoritem from './assets/image/amr-koritem.png'
 import freeLance from './assets/json/free-lance.json'
 import inova from './assets/json/inova.json'
 import openSource from './assets/json/open-source.json'
@@ -9,8 +10,31 @@ import AllAchievementsView from './components/AllAchievementsView'
 import { Achievement } from './models/Achievement'
 import AchievementHeader from './components/AchievementHeader'
 
+function InfoHeader() {
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <img src={amrKoritem} style={{
+        objectPosition: "left top",
+        objectFit: "contain",
+        width: "15%",
+        minWidth: "200px",
+        margin: "10px"
+      }} />
+      <div style={{ marginLeft: "10px" }}>
+        <h2>Amr Mohamed Mohamed Koritem</h2>
+        <p>I'm a mobile developer who's both hardworking and passionate about creating and developing new solutions for every day problems using software.</p>
+        <p>Working since 2019. Mainly, as iOS but have also worked as:</p>
+        <ul>
+          <li>Flutter developer</li>
+          <li>React Native developer</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
 function groupingView() {
-  let swiftLibs = openSource.swift
+  const swiftLibs = openSource.swift
   return (
     <>
       <AchievementHeader title="Libraries" />
@@ -25,12 +49,20 @@ function groupingView() {
 function App() {
   let [isGroupingMode, setIsGroupingMode] = useState(false)
   if (isGroupingMode) {
-    return groupingView()
+    return (
+      <>
+        {InfoHeader()}
+        {groupingView()}
+      </>
+    )
   }
   const libs = openSource.swift as Achievement[]
   const allAchievements = libs.concat(vois.ios).concat(freeLance.ios).concat(inova.ios)
   return (
-    <AllAchievementsView achievements={allAchievements} flutterApps={inova.flutter} />
+    <>
+      {InfoHeader()}
+      <AllAchievementsView achievements={allAchievements} flutterApps={inova.flutter} />
+    </>
   )
 }
 
--- react-code/src/components/AchievementHeader.tsx
+++ react-code/src/components/AchievementHeader.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const AchievementHeader: React.FC<{title: string}> = (props) => {
     return (
-        <h3 style={{ marginRight: "10px", marginLeft: "10px" }}>{props.title}</h3>
+        <h3 style={{marginRight: "10px", marginLeft: "10px"}}>{props.title}</h3>
     )
 }
 
