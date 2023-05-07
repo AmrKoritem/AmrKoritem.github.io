57cbe6cb63bc9b424f474f109e2977c80538ae5a
Amr Koritem <amr.koritem92@gmail.com>
2023-05-08 01:10:06 +0300

    home description modified

--- react-code/src/components/home_views/HomeDescription.tsx
+++ react-code/src/components/home_views/HomeDescription.tsx
@@ -3,10 +3,10 @@ import React from "react"
 const HomeDescription: React.FC = (props) => {
     return (
         <div>
-            <p>I'm a</p>
-            <h1>Mobile Developer</h1>
             <p>My name is</p>
             <h1>Amr Koritem</h1>
+            <p>I'm a</p>
+            <h1>Mobile Developer</h1>
         </div>
     )
 }
